6e18ca55e06996f05cfe81e9f708733cd7d203f4
JustPugDev <65760447+JustPugDev@users.noreply.github.com>
2020-07-14 04:19:34 +1000

    Main Build

---
+++ start.bat
@@ -0,0 +1,1 @@
+java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -Dfile.encoding=UTF-8 -jar bin.jar
